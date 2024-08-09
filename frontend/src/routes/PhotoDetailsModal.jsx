ade0a3da1fd85896b35b080c2a8be88df2772f34
Sampson Chang <sampson.chang@gmail.com>
2024-08-09 16:48:50 -0700

    added similar photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -5,16 +5,36 @@ import closeSymbol from '../assets/closeSymbol.svg';
 
 const PhotoDetailsModal = (props) => {
   const { photo, onClose } = props
-
   if (!photo) return null;
   
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" onClick={onClose}/>
+      <button className="photo-details-modal__close-button" onClick={onClose}>
+        <img src={closeSymbol} alt="close symbol" />
       </button>
-        <img src={photo.fullimg} alt={photo.id} className="photo-details-modal__images" />
-        <p>{photo.id}</p>
+      <div className='photo-details-modal__images'>
+
+      <img src={photo.fullimg} alt={photo.id} className="photo-details-modal__image" />
+      </div>
+
+      {console.log(photo.similarPhotos)}
+
+      <div className="photo-details-modal__photographer-details photo-details-modal__header ">
+        <img src={photo.user.profile} 
+        alt={`${photo.user.username}'s profile`} 
+        className='photo-details-modal__photographer-profile'/>
+
+        <p className="photo-details-modal__photographer-info">{photo.user.username}</p>
+
+        <p className="photo-details-modal__photographer-location">{`${photo.location.city}, ${photo.location.country}`}</p>
+      </div>
+      <div className='photo-details-modal__images'>
+        {Object.values(photo.similarPhotos).map((photo) => (
+          <span key={photo.id} >
+            <img src={photo.urls.regular} alt={photo.id} className='photo-details-modal__image'/>
+          </span>
+        ))}
+      </div>
     </div>
   )
 };
